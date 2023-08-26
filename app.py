e4f725fe573aa07bc6c2781c4777e78811a2e6ce
soohian <shonendumm@gmail.com>
2023-08-26 12:10:57 +0800

    add with app.appcontext()

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 import time
@@ -8,40 +8,39 @@ logger = logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-scheduler = BackgroundScheduler()
-
-
 # Configure the logger for the Flask app
 app.logger.setLevel(logging.INFO)  # Set the desired level
 
-# Initialize a global variable to store data
-data = "Initial Data"
+scheduler = BackgroundScheduler()
 
-def fetch_data():
-    # Replace with your data-fetching logic
-    # time.sleep(20) # simulate time to get data
-    global data 
-    data = f"Data: {time.strftime('%Y-%m-%d %H:%M:%S')}"
-    app.logger.info(f'data = {data}')
+with app.app_context() as g:
 
-def schedule_fetch_data():
-    scheduler.add_job(fetch_data, 'interval', seconds=30)
-    scheduler.start()
+    # Initialize a global variable to store data
+    g.data = "Initial Data"
 
-# Start fetching data immediately upon starting the app
-fetch_data()
+    def fetch_data():
+        # Replace with your data-fetching logic
+        # time.sleep(20) # simulate time to get data
+        g.data = f"Data: {time.strftime('%Y-%m-%d %H:%M:%S')}"
 
-# Start the scheduler
-schedule_fetch_data()
+    def schedule_fetch_data():
+        scheduler.add_job(fetch_data, 'interval', seconds=30)
+        scheduler.start()
+
+    # Start fetching data immediately upon starting the app
+    fetch_data()
+
+    # Start the scheduler
+    schedule_fetch_data()
 
 
 @app.route("/")
 def home():
-    return render_template('index.html', data=data)
+    return render_template('index.html', data=g.data)
 
 
 if __name__ == "__main__":
     try:
         app.run()
-    except KeyboardInterrupt:
+    except:
         scheduler.shutdown()  # Properly shutdown the scheduler
